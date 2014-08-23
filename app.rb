a78a841ef962a0889baba9d05f731066e73ba9ad
Ye Lin Aung <me@yelinaung.com>
2014-08-23 22:21:54 +0630

    single quote and remove extra lines

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  "Hello, World!"
+  'Hello, World!'
 end
-
--- test/hello_test.rb
+++ test/hello_test.rb
@@ -1,7 +1,7 @@
 require File.expand_path '../test_helper.rb', __FILE__
 
-class MyTest < MiniTest::Unit::TestCase
-
+# Testing Hello World
+class HelloTest < MiniTest::Unit::TestCase
   include Rack::Test::Methods
 
   def app
@@ -11,6 +11,6 @@ class MyTest < MiniTest::Unit::TestCase
   def test_hello_world
     get '/'
     assert last_response.ok?
-    assert_equal "Hello, World!", last_response.body
+    assert_equal 'Hello, World!', last_response.body
   end
 end
